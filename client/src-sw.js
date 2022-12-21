af445b59877a1b4df1ed329f7e83f9b1f9be21c5
swnova <sharrodwillanova@gmail.com>
2022-12-21 01:08:43 -0800

    reclone repo + client side code

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate  } = require('workbox-strategies');
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -29,8 +29,10 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(({request})=> ['style', 'script', 'worker'].includes(request.destination),
 new StaleWhileRevalidate({
+  // Name of the cache storage.
   cacheName: 'asset-cache',
   plugins: [
+    // This plugin will cache responses with these headers to a maximum-age of 30 days
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,11 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+
+    // Store the triggered events
     window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
 });
 
@@ -12,14 +16,20 @@ butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
-        return;
+     return;
     }
+  
+    // Show prompt
     promptEvent.prompt();
+    
+    // Reset the deferred prompt variable, it can only be used once.
     window.deferredPrompt = null;
+    
     butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+     // Clear prompt
+  window.deferredPrompt = null;
 });
