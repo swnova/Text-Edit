9c8a9b761bc5276c405d36c56b3fa551be22a274
swnova <sharrodwillanova@gmail.com>
2022-12-21 00:35:38 -0800

    push to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const {StaleWhileRevalidate} = require('workbox-strategies');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate  } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -34,7 +33,10 @@ new StaleWhileRevalidate({
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
-    })
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
   ],
 })
 );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -20,7 +18,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'contact cards'
+        title: 'Webpack'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -29,11 +27,18 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'contact cards',
+        name: 'jate',
         short_name: 'jate',
         description: 'just another text editor',
-        start_url: './',
-        publicPath:'./',
+        start_url: '/',
+        publicPath:'/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
       
     ],
